2fc288a91ed7c28270ce0e74327cb68c46e5338b
Marcin Malek <marcinmalek01@gmail.com>
2022-07-11 22:48:35 +0200

    added background, formatted code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import { IntensitySwitch } from './IntensitySwitch';
 import { ReactComponent as LightIcon } from "./icons/icon-4-5_light.svg";
 import { ReactComponent as ModerateIcon } from "./icons/icon-4-5_moderate.svg";
 import { ReactComponent as HeavyIcon } from "./icons/icon-4-5_heavy.svg";
+import { Background } from './styled';
 
 function App() {
   return (
-    <IntensitySwitch title="Fog" lightIcon={<LightIcon />} moderateIcon={<ModerateIcon />} heavyIcon={<HeavyIcon />} />
+    <Background>
+      <IntensitySwitch
+        title="Fog"
+        lightIcon={<LightIcon />}
+        moderateIcon={<ModerateIcon />}
+        heavyIcon={<HeavyIcon />}
+      />
+    </Background>
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+    box-sizing: border-box;
+    padding: 100px;
+    background-color: #111737;
+`;
