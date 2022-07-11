0bcd8fa42ee5b921016877b745a07be6db160b70
Marcin Malek <marcinmalek01@gmail.com>
2022-07-11 22:51:11 +0200

    button styling

--- src/IntensitySwitch/styled.js
+++ src/IntensitySwitch/styled.js
@@ -2,17 +2,31 @@ import styled from "styled-components";
 
 export const Label = styled.span`
     font-size: 15px;
+    font-weight: 600;
+    color: white;
 `;
 
 export const Button = styled.button`
     display: flex;
+    cursor: pointer;
+    width: 130px;
+    height: 130px;
+    padding: 5px 10px;
+    margin: 5px 0;
     flex-direction: column;
     background-color: black;
     border-radius: 5px;
-    color: ${({active}) => active ? "white" : "gray"};
-    outline: ${({active}) => active && "1px solid"};
+    color: ${({ active }) => active ? "white" : "gray"};
+    border: ${({ active }) => active && "2px solid"};
     align-items: flex-start;
-    justify-content: center;
+    justify-content: flex-end;
+    transition: 0.2s;
+
+    & svg {
+        align-self: center;
+        width: 90px;
+        height: 90px;
+    }
 
     & path {
         fill: currentColor;
